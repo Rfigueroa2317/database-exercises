e129a5759530e623462cf08d9dbf61b302b05456
ricardofigueroa <rfigueroa2317@gmail.com>
2021-07-27 12:22:27 -0500

    update

--- albums_seeder.sql
+++ albums_seeder.sql
@@ -1,3 +1,15 @@
 USE codeup_test_db;
 
-INSERT
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE albums (id int UNSIGNED NOT NULL AUTO_INCREMENT,
+                    artist VARCHAR (50) NOT NULL,
+                    name VARCHAR (50) NOT NULL,
+                    sales FLOAT,
+                    genre VARCHAR (50),
+                    PRIMARY KEY (id));
+
+
+DESCRIBE albums;
+
+SELECT * FROM albums;
