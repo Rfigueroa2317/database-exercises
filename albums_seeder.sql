7eff64bad65ee6cb5e359c6456596f0aeb94c358
ricardofigueroa <rfigueroa2317@gmail.com>
2021-07-27 14:29:58 -0500

    update

--- albums_seeder.sql
+++ albums_seeder.sql
@@ -9,6 +9,13 @@ CREATE TABLE albums (id int UNSIGNED NOT NULL AUTO_INCREMENT,
                     genre VARCHAR (50),
                     PRIMARY KEY (id));
 
+INSERT INTO albums (
+    artist, name, sales, genre,release_date)
+VALUES ('Meat Loaf','Bat Out of Hell',21.7,'Hard rock, glam rock, progressive rock','1977-01-01'),(
+    'Eagles','Their Greatest Hits',41.2,'Country rock,soft rock,folk rock','1976-01-01'),(
+           'Whitney Houston','The Bodyguard',32.4,'R&B, soul, pop, soundtrack','1992-01-01'
+       );
+
 
 DESCRIBE albums;
 
