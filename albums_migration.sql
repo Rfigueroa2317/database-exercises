e20835aae7bd61244a5c583e8dce7a22ac5fc02f
ricardofigueroa <rfigueroa2317@gmail.com>
2021-07-27 09:30:54 -0500

    update

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,3 +1,14 @@
 USE codeup_test_db;
+DROP TABLE IF EXISTS albums;
+CREATE TABLE albums (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist VARCHAR (50) NOT NULL,
+    name VARCHAR (50) NOT NULL,
+    release_date DATE NOT NULL,
+    sales FLOAT,
+    genre VARCHAR (50),
+    PRIMARY KEY (id));
 
+DESCRIBE albums;
 
+SELECT * FROM albums;
