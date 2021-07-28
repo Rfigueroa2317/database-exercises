78eb14e6fd536a25ddcf50a1983d825ff76cebc0
ricardofigueroa <rfigueroa2317@gmail.com>
2021-07-27 21:45:16 -0500

    update

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,3 +1,15 @@
 USE codeup_test_db;
 
 DROP TABLE IF EXISTS albums;
+
+CREATE TABLE albums (id int UNSIGNED NOT NULL AUTO_INCREMENT,
+                     artist VARCHAR (50) NOT NULL,
+                     name VARCHAR (50) NOT NULL,
+                     sales FLOAT,
+                     genre VARCHAR (50),
+                     release_date DATE NOT NULL,
+#                      discography VARCHAR(100) NOT NULL,
+                     PRIMARY KEY (id));
+
+
+SELECT artist, name FROM albums;
