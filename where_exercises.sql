484bfc3c11758fdb76a1ea1d61436afa39828e9b
ricardofigueroa <rfigueroa2317@gmail.com>
2021-07-29 09:28:18 -0500

    update

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,4 +1,13 @@
 USE employees;
 
-SELECT * FROM employees WHERE first_name IN ('Irena','Vidya','Maya');
-SELECT * FROM employees WHERE last_name LIKE ('E%');
+# SELECT * FROM employees WHERE first_name IN ('Irena','Vidya','Maya');
+# SELECT * FROM employees WHERE last_name LIKE ('E%');
+# SELECT * FROM employees WHERE last_name LIKE ('%q%');
+
+
+# SELECT * FROM employees WHERE first_name = ('Irena') OR ('Vidya') OR ('Maya');
+# SELECT first_name, gender FROM employees WHERE gender = 'Male' AND first_name = ('Irena') OR ('Vidya') OR ('Maya');
+SELECT * FROM employees WHERE last_name LIKE ('E%') OR ('%E');
+SELECT * FROM employees WHERE last_name LIKE ('E%') AND ('%E');
+SELECT * FROM employees WHERE last_name LIKE ('q%');
+
