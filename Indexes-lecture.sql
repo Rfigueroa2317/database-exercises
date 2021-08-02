55fa217c048a809b8508e3fa0686cb64c7c5ba83
ricardofigueroa <rfigueroa2317@gmail.com>
2021-08-02 09:35:21 -0500

    class notes

---
+++ Indexes-lecture.sql
@@ -0,0 +1,17 @@
+Indexes
+
+primary keys;
+    . Must be on the table independently
+    . Highlander rule (there can only be one)
+    . Optimizes search
+    . Can be more than 1 column
+
+Foreign keys;
+    . Must primarily exist on another table
+    . Used so our table doesnt have to maintain out-of-scope something
+    . Tables and fk are dependent on Fks pk table
+
+Unique;
+    . Can be any column
+    . Completely index of other tables
+    . Gives perform boost
