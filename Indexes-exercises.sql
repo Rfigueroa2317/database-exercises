81a9366bab14ed8bba6c47f88497ea21fe130ea6
ricardofigueroa <rfigueroa2317@gmail.com>
2021-08-02 10:54:02 -0500

    update

--- Indexes-exercises.sql
+++ Indexes-exercises.sql
@@ -2,7 +2,7 @@ USE employees_db;
 
 CREATE TABLE employees_db (
     id INT NOT NULL AUTO_INCREMENT,
-    author VARCHAR(50) NOT NULL,
+    employee VARCHAR(50) NOT NULL,
     content VARCHAR(240) NOT NULL,
     PRIMARY KEY (id)
 )
