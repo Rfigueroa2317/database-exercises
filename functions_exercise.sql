abd91400474d49ce546edd19a247f457d40f913a
ricardofigueroa <rfigueroa2317@gmail.com>
2021-08-30 22:37:26 -0500

    working on functions

---
+++ functions_exercise.sql
@@ -0,0 +1,31 @@
+USE employees;
+
+SELECT * FROM employees WHERE first_name IN ('Irena','Vidya','Maya') ORDER BY first_name;
+SELECT * FROM employees WHERE first_name IN ('Irena','Vidya','Maya') ORDER BY first_name, last_name;
+SELECT * FROM employees WHERE first_name IN ('Irena','Vidya','Maya') ORDER BY last_name,first_name;
+SELECT * FROM employees WHERE first_name IN ('Irena','Vidya','Maya') ORDER BY last_name DESC, first_name DESC;
+
+SELECT * FROM employees WHERE last_name LIKE 'E%' ORDER BY emp_no;
+SELECT * FROM employees WHERE last_name LIKE 'E%' ORDER BY emp_no;
+
+
+
+
+
+-- # SELECT * FROM employees WHERE last_name LIKE ('E%');
+-- # SELECT * FROM employees WHERE last_name LIKE ('%q%');
+-- # today's push
+
+
+# SELECT * FROM employees WHERE first_name = ('Irena') OR ('Vidya') OR ('Maya');
+# SELECT first_name, gender FROM employees WHERE gender = 'Male' AND first_name = ('Irena') OR ('Vidya') OR ('Maya');
+SELECT * FROM employees WHERE last_name LIKE ('E%') OR ('%E');
+SELECT * FROM employees WHERE last_name LIKE ('E%') AND ('%E');
+SELECT * FROM employees WHERE last_name LIKE ('q%');
+SELECT * FROM employees WHERE last_name LIKE ('qu%');
+
+
+
+
+
+
