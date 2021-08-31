c23c755a3fe0d611194d89064e5970ac6b9a4a4f
ricardofigueroa <rfigueroa2317@gmail.com>
2021-08-30 22:15:43 -0500

    update

--- limit-exercises.sql
+++ limit-exercises.sql
@@ -1,1 +1,12 @@
 -- DUE ON WEDNESDAY THE LATEST! --
+USE employees;
+SELECT * FROM employees;
+
+# 2
+SELECT DISTINCT last_name FROM employees ORDER BY last_name DESC, last_name LIKE 'Z%' LIMIT 10;
+
+
+# 3
+SELECT salary FROM salaries WHERE emp_no LIMIT 5 OFFSET 1;
+
+SELECT salary FROM salaries;
