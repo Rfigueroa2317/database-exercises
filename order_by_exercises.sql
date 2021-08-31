d17f454f1e1714956228ccd4bfc344280b5a5785
ricardofigueroa <rfigueroa2317@gmail.com>
2021-08-31 15:28:23 -0500

    refactoring and correcting files

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -1,37 +1,28 @@
 USE employees;
 
 #2
-SELECT CONCAT(first_name, ' ', last_name)
+SELECT DISTINCT title
+FROM titles;
+
+#3 & 4
+SELECT first_name, last_name
 FROM employees
 WHERE last_name LIKE 'E%E'
-ORDER BY emp_no DESC;
-SELECT emp_no, birth_date, CONCAT(first_name, ' ', last_name) AS 'first_name_last_name', gender, hire_date
+GROUP BY first_name, Last_name
+ORDER BY first_name, last_name;
+
+#5 & 6
+SELECT COUNT(*), last_name
 FROM employees
-WHERE last_name LIKE 'E%E'
-ORDER BY emp_no DESC;
+WHERE last_name LIKE '%q%'
+  AND last_name NOT LIKE '%qu%'
+GROUP BY last_name
+ORDER BY COUNT(*);
 
-#3
-SELECT *
+#7
+SELECT COUNT(*), gender
 FROM employees
-WHERE MONTH(birth_date) = 12
-  AND DAY(birth_date) = 25;
-
-#4 & 5
-SELECT *
-FROM employees
-WHERE MONTH(birth_date) = 12
-  AND DAY(birth_date) = 25
-  AND YEAR(hire_date) BETWEEN 1990 AND 1999
-ORDER BY birth_date, hire_date DESC;
-
-#6
-SELECT *, DATEDIFF(CURDATE(), hire_date) AS 'days_worked'
-FROM employees
-WHERE MONTH(birth_date) = 12
-  AND DAY(birth_date) = 25
-  AND YEAR(hire_date) LIKE '199%'
-ORDER BY birth_date, hire_date DESC;
-
-
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+GROUP BY gender;
 
 
