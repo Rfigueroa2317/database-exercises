8a7b6ff2bcf94dd2677e575d224fc57588d145ed
ricardofigueroa <rfigueroa2317@gmail.com>
2021-08-31 19:28:58 -0500

    finishing homework

---
+++ aliases_exercises.sql
@@ -0,0 +1,17 @@
+USE employees;
+
+#3
+SELECT CONCAT(last_name, ' ', first_name) AS full_name
+FROM employees
+LIMIT 10;
+
+#4
+SELECT CONCAT(last_name, ' ', first_name) AS full_name, birth_date AS DOB
+FROM employees
+LIMIT 10;
+
+#5
+SELECT CONCAT(emp_no, ' - ', last_name, ' ', first_name) AS full_name, birth_date AS DOB
+FROM employees
+LIMIT 10;
+
